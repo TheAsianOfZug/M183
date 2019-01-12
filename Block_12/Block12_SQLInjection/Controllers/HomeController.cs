ee2a6681118d29905e5a4fde10fd7ed285a3c875
TheAsianOfZug <denis.huelin@gmail.com>
2019-01-12 17:09:00 +0100

    Add Missing Projects

---
+++ Block_12/Block12_SQLInjection/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Block12_SQLInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        { 
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DoLogin()
+        {
+            var username = Request["username"];
+            var password = Request["password"];
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\gibz\\Documents\\sql_xss_injection.mdf\";Integrated Security=True;Connect Timeout=30";
+
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader dataReader;
+
+            cmd.CommandText = "SELECT [Id] ,[username] ,[password] FROM [dbo].[User] WHERE [username] = '"+username+"' AND [password] = '" + password + "'";
+            cmd.Connection = con;
+
+            con.Open();
+            dataReader = cmd.ExecuteReader();
+            if (dataReader.HasRows)
+            {
+                ViewBag.Message = "success";
+                while (dataReader.Read())
+                {
+                    ViewBag.Message += dataReader.GetInt32(0) + " " + dataReader.GetString(1) + " " + dataReader.GetString(2);
+                }
+            }
+            else
+            {
+                ViewBag.Message = "nothing to read of";
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Feedback()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DoFeedback()
+        {
+            var feedback = Request["feedback"];
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\gibz\\Documents\\sql_xss_injection.mdf\";Integrated Security=True;Connect Timeout=30";
+
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader dataReader;
+
+            cmd.CommandText = "INSERT INTO [dbo].[Feedback] SET [feedback = '" + feedback + "'"
+            cmd.Connection = con;
+
+            con.Open();
+            dataReader = cmd.ExecuteReader();
+            if (dataReader.HasRows)
+            {
+                ViewBag.Message = "success";
+                while (dataReader.Read())
+                {
+                    ViewBag.Message += dataReader.GetInt32(0) + " " + dataReader.GetString(1);
+                }
+            }
+            else
+            {
+                ViewBag.Message = "nothing to read of";
+            }
+            return View();
+        }
+    }
+}
---
+++ Block_8/Block7_Session/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Block7_Session.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login()
+        {
+            string username = Request["username"];
+            string password = Request["password"];
+            string eingeloggtbleiben = Request["eingeloggtbleiben"];
+
+            if ("michaeljakober".Equals(username) && "123456".Equals(password))
+            {
+                ViewBag.Message = "Successfully logged in!";
+                if (eingeloggtbleiben == "J")
+                {
+                    HttpCookie cookie = new HttpCookie("Sessioncookie", "Logged in");
+                    cookie.Expires = DateTime.Now.AddDays(14);
+                }
+                else
+                {
+                    Session["Logged in"] = true;
+                }
+            }
+            else
+            {
+                ViewBag.Message = "Bad Credentials!";
+            }
+            return View();
+        }
+    }
+}
